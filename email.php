f692b3d9046e7e6ff4b52ef722a80bc87e45c472
LR222222 <er339467@truro-penwith.ac.uk>
2023-12-05 09:42:24 +0000

    attempt to fix token

--- email.php
+++ email.php
@@ -1,10 +1,12 @@
 <?php 
-include_once "user.php"
+include_once "user.php";
+// $sql = "SELECT token FROM users WHERE email = " .  $_POST['email'];
 ?>
 
 <h1>Welcome to the site!</h1>
 <br />
-<!-- something wrong here -->
-<a href="log-in.localhost/verify-email.php?token=<?=$user->token?>">Verify your email</a>
+
+<a href="log-in.localhost/verify-email.php?token={<?= $user->token ?>}">Verify your email</a>
+
 <br />
 <p>This message has been sent because you signed up to log-in.localhost</p>
